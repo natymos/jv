88efb2abbf60333084a2d787a682384a09c91046
natymos <natymos@yahoo.com>
2023-12-27 12:38:46 -0500

    Deber ejercicio2

---
+++ CalculadoraPromedio.js
@@ -0,0 +1,41 @@
+/*let nombre="Nombre"
+let materia="Materia"
+let nota1="x"
+
+if (nota1>=0 && nota1<=10){
+    console.log("Ingresar calificación 2")
+else if (nota1<0 && nota1>10){
+    console.log("Error, ingresar calificación nuevamente")
+}
+}
+
+let nota2="y"
+if (nota2>=0 && nota2<=10){
+    console.log("Ingresar calificación 3")
+else if (nota2<0 && nota2>10){
+    console.log("Error, ingresar calificación nuevamente")
+}
+}
+
+
+let nota3="z"
+if (nota3<0 && nota3>10){
+    console.log("Error, ingresar calificación nuevamente")
+
+while (nota3>=0 && nota3<=10){
+    p=(nota1+nota2+nota3)/3
+    if(p>=7){
+        console.log(nombre + "Felicidades!, Has aprobado con un promedio de: "+p)
+        else if (p<7){
+            console.log(nombre + "Gracias por tu esfuerzo. Nos vemos pronto en clase. El promedio obtenido es:" +p)
+
+        }
+
+    }
+
+}
+  
+}
+}*/
+a=prompt("Ingresar nombre del estudiante")}
+console.log(Estudiante)
