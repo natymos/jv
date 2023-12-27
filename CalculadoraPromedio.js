21dec0d0433ffddf0d36591c701ed577a1f2e69d
natymos <natymos@yahoo.com>
2023-12-27 18:40:35 -0500

    prueba hola mundo

--- CalculadoraPromedio.js
+++ CalculadoraPromedio.js
@@ -1,3 +1,25 @@
+console.log("hola mundo")
+
+
+/*var nombrealumno=prompt ("Ingresa el nombre del alumno")
+var materia=prompt ("Ingresa la materia")
+
+var nota1=parseFloat(prompt ("Ingresa la calificación1"))
+var nota2=parseFloat(prompt ("Ingresa la calificación2"))
+var nota3=parseFloat(prompt ("Ingresa la calificación3"))
+
+if (isNaN(nota1)|| isNaN(nota2)||isNaN(nota3)||nota1<0||nota1>10||nota2<0||nota2>10||nota3<0||nota3>10){
+    console.log("Por favor, ingrese una nota válida entre 0 y 10")
+}
+let promedio= (nota1+nota2+nota3)/3
+
+if (promedio>=7){
+    console.log("Felicidades ${nombrealumno}, aprobaste ${materia} con un promedio de: ${promedio}")
+}    
+    else if(promedio<7){
+      console.log("Gracias por el esfuerzo ${nombrealumno}, nos vemos pronto en clase. El promedio es: ${promedio}")
+}
+*/
 /*let nombre="Nombre"
 let materia="Materia"
 let nota1="x"
@@ -36,6 +58,6 @@ while (nota3>=0 && nota3<=10){
 }
   
 }
-}*/
+}
 a=prompt("Ingresar nombre del estudiante")}
-console.log(Estudiante)
+console.log(Estudiante)*/
